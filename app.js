c9e2b1749008b14a3e71f0382dc63068325b7946
Abdoul Dgit config --global user.email abdoulson02@gmail.com <abdoulson02@gmail.com>
2021-01-26 12:28:10 -0500

    added javaScript variables

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName = "Abdoul D";
+console.log(firstName);
+
+let lastName = "Guibila";
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+// Exercise 2
+let fullName = `${firstName} ${lastName}` // firstName + LastName
+console.log(fullName);
+
+// Exercise 3
+let myStory;
+let city = "New York city";
+let pastime = "like to read true story Books "
+//console.log(fullName + " " + "live in" + " " + city + " " + "and" + pastime)
+console.log(`${fullName} live in ${city} and ${pastime}`);
